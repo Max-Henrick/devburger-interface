e16b95527fb8d6ed8f0fb4818d2462c46ca2a7ed
Max-Henrick <maxenrick23@gmail.com>
2023-06-02 10:05:57 -0300

    config/eslint + config/prettier

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 import Login from './containers/Login'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<Login/>
-);
-
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Login />)
