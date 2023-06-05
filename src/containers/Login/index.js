4dc2c1e189704666bd06ba7f183aab0de5eab4f0
Max-Henrick <maxenrick23@gmail.com>
2023-06-05 10:37:46 -0300

    criação e estilo da pagina de Login

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,10 +1,38 @@
 import React from 'react'
 
+import LoginImg from '../../assets/Login-image.svg'
+import Logo from '../../assets/Logo-image.svg'
+import {
+  Container,
+  ContainerItens,
+  LoginImage,
+  Label,
+  Input,
+  Button,
+  P
+} from '../../containers/Login/styles'
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LoginImage src={LoginImg} alt="Login-image" />
+      <ContainerItens>
+        <img src={Logo} alt="Logo" />
+        <h1>Login</h1>
+
+        <Label>Email</Label>
+        <Input />
+
+        <Label>Senha</Label>
+        <Input />
+
+        <Button>Entrar</Button>
+
+        <P>
+          Não possui conta? <a>Cadastre-se</a>
+        </P>
+      </ContainerItens>
+    </Container>
   )
 }
 
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,99 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const LoginImage = styled.img`
+  height: 100%;
+  width: 50%;
+`
+
+export const ContainerItens = styled.div`
+  background: #373737;
+  height: 100%;
+  width: 50%;
+  padding: 25px 175px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 28px;
+    color: #ffffff;
+    text-align: center;
+    margin-top: 50px;
+  }
+
+  img {
+    width: 338px;
+    height: 125px;
+  }
+`
+
+export const Label = styled.p`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 14px;
+  color: #ffffff;
+  margin-top: 25px;
+  margin-bottom: 5px;
+  padding-left: 5px;
+`
+
+export const Input = styled.input`
+  background: #ffffff;
+  box-shadow: 3px 3px 10px rgba(74, 144, 226, 0.19);
+  border-radius: 5px;
+  border: none;
+  padding-left: 5px;
+  width: 338.42px;
+  height: 38.32px;
+`
+
+export const Button = styled.button`
+  width: 182px;
+  height: 36px;
+  border: none;
+  background: #9758a6;
+  border-radius: 20px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  color: #eeeeee;
+  cursor: pointer;
+  margin-top: 66px;
+  margin-bottom: 28px;
+
+  &:hover {
+    opacity: 0.7;
+  }
+  &:active {
+    opacity: 0.5;
+  }
+`
+
+export const P = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+  color: #ffffff;
+  a {
+    text-decoration: underline;
+    cursor: pointer;
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+`
